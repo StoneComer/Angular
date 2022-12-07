42f2562e9372d470e07e3d954f2b4b3ca2ef7092
StoneComer <fominm8@gmail.com>
2022-12-07 18:48:31 +0300

    zadanie2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'FirstEvoApp';
+  title = 'Zadanie2';
+  date_now = new Date();
+  num = Math.floor(Math.random() * (95 - 0 + 1)) + 0;
+  array1 = [
+    {
+      name: '1 element'
+    },
+    {
+      name: '2 element'
+    },
+    {
+      name: '3 element'
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StringPipePipe } from './string-pipe.pipe';
+import { Zadanie2Pipe } from './zadanie2.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StringPipePipe,
+    Zadanie2Pipe
   ],
   imports: [
     BrowserModule,
