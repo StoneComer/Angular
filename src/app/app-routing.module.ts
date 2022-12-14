c10032f0132e6d964f78eb66072eb7b4dbadd3d5
StoneComer <fominm8@gmail.com>
2022-12-14 19:17:37 +0300

    Revert "zadanie3"
    This reverts commit d783aea19f7f70e2b63d92a4d656b8f4d6a6b5f0.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,40 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { ItemComponent } from './item/item.component';
-import { ItemDetailsComponent } from './item/item-details/item-details.component';
-import { ItemListComponent } from './item/item-list/item-list.component';
-import { AppComponent } from './app.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent
-  },
-  {
-    path: 'about',
-    component: AboutComponent
-  },
-  {
-    path: 'contact',
-    component: ContactComponent
-  },
-  {
-    path: 'item/:id',
-    component: ItemComponent,
-    children: [
-      {
-        path: 'details',
-        component: ItemDetailsComponent
-      },
-      {
-        path: 'list',
-        component: ItemListComponent,
-      }
-    ]
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
